e2eaf1fae82d33d9dbaf6ba00a4e68e398ba2288
hvela <hvela@users.noreply.github.com>
2019-05-02 17:23:37 -0400

    Refactored file naming conventions

---
+++ application.py
@@ -0,0 +1,71 @@
+import sqlite3
+# from Shows2 import Movies, Implementation
+from shows_movies import Flix
+import random
+
+class FlixStorage:
+    # step 1 connect the database
+    conn = sqlite3.connect('databaseFile.db')
+
+    # step 2 create the cursor
+    c = conn.cursor()
+
+    # step 3 create the table
+    c.execute('CREATE TABLE IF NOT EXISTS movies (movie_name TEXT)')
+
+    c.execute("""CREATE TABLE IF NOT EXISTS shows (showname TEXT)""")
+
+    # prompt start of program
+    Flix.start_program(None)
+
+    # insert data to the shows table here
+    c.execute('INSERT INTO shows VALUES (?)', (Flix.add_show(None),))
+
+    # Insert data into the movies table
+    c.execute('INSERT INTO movies VALUES (?)', (Flix.add_movie(None),))
+
+    # get and print all the movies -------
+    print("The movies in the pool are: ")
+    c.execute("SELECT * FROM movies")
+    print(c.fetchall())
+
+    # get all the movies
+    d = c.execute("SELECT * FROM movies")
+
+    max = c.execute("SELECT max(rowid) FROM movies")
+    # get and print number of movies
+    n = c.fetchone()[0]
+    print(n)
+
+    # variable to randomize movie
+    i = random.randrange(0, n)
+
+    c.execute("SELECT*FROM movies WHERE rowid={}".format(i))
+    # set variable to what has been chosen
+    pick = c.fetchone()
+    # print the random pick
+    print('The movie to watch is: ', pick)
+
+    # print all the shows --------
+    print("\nThe shows in the pool are: ")
+    c.execute("SELECT * FROM shows")
+    print(c.fetchall())
+
+    for row in c.fetchall():
+        print(row)
+
+    # get all the shows
+    d = c.execute("SELECT * FROM shows")
+
+    # get all shows and print
+    max = c.execute("SELECT max(rowid) FROM shows")
+    n = c.fetchone()[0]
+    print(n)
+
+    # get random show and store in variable
+    i = random.randrange(1,n-1)
+
+    # print the random show
+    c.execute("SELECT*FROM shows WHERE rowid={}".format(i))
+    pick_show = c.fetchone()[0]
+    print('The show to watch is: ',pick_show)
---
+++ shows_movies.py
@@ -0,0 +1,42 @@
+import random
+import json
+
+
+class Flix:
+    #constructor method that has variables
+    def __init__(self, showname=None, moviename=None):
+        self.showname = showname
+        self.moviename = moviename
+
+    def start_program(self):
+        print('{}\nThis program will allow you to enter shows and movies and store'
+              ' them in a database.\nThen it will print out 1 show and movie randomly'
+              ' that you have stored.\n'.format('d*_*b'
+                                             '\n  |'
+                                              '\n - -\n\n'))
+
+    def add_show(self):
+        try:
+            showname = input('What is the name of the show: ')
+            if not showname:
+                raise ValueError
+        except ValueError:
+            print('You did not enter a show name')
+
+        return showname
+
+    def add_movie(self):
+        try:
+            moviename = input('What is the name of the movie: ')
+            if not moviename:
+                raise ValueError
+        except ValueError:
+            print("You did not enter a movie name")
+        return moviename
+
+
+
+
+
+
+
