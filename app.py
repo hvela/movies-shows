1a4b4b5c24f2bf059ccd4ee4434564c95b808fce
hvela <hvela@users.noreply.github.com>
2019-05-08 23:34:47 -0400

    fixing pep 8 errors

--- app.py
+++ app.py
@@ -10,28 +10,27 @@ class Menu(FlixStorage):
 
     def start_program():
         while True:
+            choice = input("\nEnter one of the following to continue\n\n"
+                           "1 - to add a movie\n"
+                           "2 - to add a show\n"
+                           "3 - to get information about any movie\n"
+                           "4 - to quit\n\n"
+                           "input: ")
 
-           choice = input("\nEnter one of the following to continue\n\n"
-                               "1 - to add a movie\n"
-                               "2 - to add a show\n"
-                               "3 - to get information about any movie\n"
-                               "4 - to quit\n\n"
-                               "input: ")
-
-           # convert to int if it's an actual number
-           if choice.isdigit():
-               choice = int(choice)
-               if choice is 1:
-                   FlixStorage.add_movies(self=None)
-               elif choice is 2:
-                   FlixStorage.add_shows(self=None)
-               elif choice is 3:
-                   api.retrieve(self=None)
-               elif choice is 4:
-                   print("Thanks for using!")
-                   break
-               else:
-                   print("You entered an invalid value, please try again.")
-           else:
-               print('error find')
+        # convert to int if it's an actual number
+            if choice.isdigit():
+                choice = int(choice)
+                if choice is 1:
+                    FlixStorage.add_movies(self=None)
+                elif choice is 2:
+                    FlixStorage.add_shows(self=None)
+                elif choice is 3:
+                    api.retrieve(self=None)
+                elif choice is 4:
+                    print("Thanks for using!")
+                    break
+                else:
+                    print("You entered an invalid value, please try again.")
+            else:
+                print('Something went wrong, try again!')
     start_program()
